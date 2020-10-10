e85af8250a38c99d08738e1cc2cc9a73bd7a93cb
MValdes13 <mvaldes13@knights.ucf.edu>
2020-10-10 13:07:42 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Col from 'react-bootstrap/Col'
+import Row  from 'react-bootstrap/Row';
+import Commons from './Commons'
+import Bathroom from './Bathroom'
+import FormControl from 'react-bootstrap/FormControl'
+
+const EVENT = {
+  id:1,
+  title:"My Birthday",
+  start: "2020-10-09",
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  <Row>
+    <Col>
+
+          <FullCalendar
+            plugins={[ dayGridPlugin ]}
+            initialView="dayGridMonth"
+            initialEvents={[EVENT]}
+          ></FullCalendar>
+        
+    </Col>
+
+    <Col md="2">
+
+      <InputGroup>
+        <InputGroup.Prepend>
+          <InputGroup.Text></InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl as="textarea" aria-label="With textarea" />
+      </InputGroup>
+    
+      <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+          Events
+        </InputGroup.Prepend>
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+          Kitchen
+        </InputGroup.Prepend>
+      </InputGroup>
+
+      <Row>
+        <Commons>
+        </Commons>
+      </Row>
+      
+      <Row>
+        <Bathroom>
+        </Bathroom>
+      </Row>
+      
+    </Col>   
+  </Row>
     </div>
   );
 }
