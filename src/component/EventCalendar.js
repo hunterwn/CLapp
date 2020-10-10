bf697d178a60db5a65520a22e66d3512fcc2daf5
Hunter Norman <hunterwnorman@gmail.com>
2020-10-10 13:22:19 -0400

    fixed errors

--- src/component/EventCalendar.js
+++ src/component/EventCalendar.js
@@ -5,18 +5,17 @@ import fire from './fire.js'
 import { INITIAL_EVENTS, createEventId } from './event-utils'
 
 
-
 class EventCalendar extends Component{
+    
 
     componentDidMount() {
-        let detailsRef = fire.database().ref('details').orderByKey();
-        detailsRef.on('child_added', snapshot => {
-          console.log(snapshot.key);
-        })
+
     }
+    
+    
 
     render() {
-
+        
         return(
             <div className = "calendar">
                 <FullCalendar
