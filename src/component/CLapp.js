fa9e2f8ba675ff1f5a015343bd4d1cb43a0ff53e
Hunter Norman <hunterwnorman@gmail.com>
2020-10-10 13:14:38 -0400

    beggining database usage

--- src/component/CLapp.js
+++ src/component/CLapp.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react'
 import EventCalendar from './EventCalendar';
 import Sidebar from './Sidebar'
-
+import fire from './fire.js'
+import { setRef } from '@material-ui/core';
 
 export default class CLapp extends Component {
 
-    
+    componentDidMount() {
+      let detailsRef = fire.database().ref('details').orderByKey();
+      detailsRef.on('child_added', snapshot => {
+        console.log(snapshot.key);
+      })
+    }
+
 
     render() {
         var views = ["Calendar", "Login"];
         var requests = ["Requests", "Pending", "History"];
         var things = ["People", "Events", "Locations"];
+
         return( 
           <div className='app-main'>
               <div className ='view'>
--- src/component/EventCalendar.js
+++ src/component/EventCalendar.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import fire from './fire.js'
+import { INITIAL_EVENTS, createEventId } from './event-utils'
+
+
 
 class EventCalendar extends Component{
+
+    componentDidMount() {
+        let detailsRef = fire.database().ref('details').orderByKey();
+        detailsRef.on('child_added', snapshot => {
+          console.log(snapshot.key);
+        })
+    }
+
     render() {
+
         return(
             <div className = "calendar">
                 <FullCalendar
                     plugins={[  dayGridPlugin  ]}
                     initialView="dayGridMonth"
+                    initialEvents={INITIAL_EVENTS}
                 />
             </div>
         )
